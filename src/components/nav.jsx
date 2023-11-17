2f0cae0ba40a4d91422690905e3edee12b54c685
Booze33 <tebetisloh@gmail.com>
2023-11-17 04:51:48 -0600

    update nav

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,15 +1,16 @@
 import { CgAdidas } from 'react-icons/cg';
 import { MdOutlineShoppingBag } from "react-icons/md";
 import { IoPersonOutline } from "react-icons/io5";
+import '../assets/styles/nav.css';
 
 const Nav = () => {
   return (
     <header className="flex absolute flex-row items-center justify-between px-4 py-2 w-screen h-16 bg-custom-color">
       <CgAdidas className="text-5xl text-white" />
       <nav className="flex space-x-4">
-        <a className="text-white text-xl px-4 font-custom">Adidas-X</a>
-        <a className="text-white text-xl px-4 font-custom">Predator</a>
-        <a className="text-white text-xl px-4 font-custom">Copa</a>
+        <a className="nav-link text-white text-xl px-4 font-custom">Adidas-X</a>
+        <a className="nav-link text-white text-xl px-4 font-custom">Predator</a>
+        <a className="nav-link text-white text-xl px-4 font-custom">Copa</a>
       </nav>
       <div className="flex space-x-4">
         <IoPersonOutline className="text-white" />
