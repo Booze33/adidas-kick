d640e33678f67273e8046230db3ae8dc3260f6f7
Tisloh Tebe <tebetisloh@gmail.com>
2023-12-16 12:45:48 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss('./tailwind.config.js')],
+  build: {
+    outDir: 'build', // or your desired output directory
+  },
 })
